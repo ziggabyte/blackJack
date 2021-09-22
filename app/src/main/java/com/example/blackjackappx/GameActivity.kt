e7eab8a88646f7f74f90bebffd533c6d3c58dca9
Ludvig Anderbeck <ludviganderbeck@gmail.com>
2021-09-22 18:16:19 +0200

    mer kort till alles

--- app/src/main/java/com/example/blackjackappx/GameActivity.kt
+++ app/src/main/java/com/example/blackjackappx/GameActivity.kt
@@ -8,6 +8,7 @@ import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import coil.load
+import com.example.blackjackappx.entities.Dealer
 import com.example.blackjackappx.entities.Deck
 import com.example.blackjackappx.entities.User
 import retrofit2.Call
@@ -15,14 +16,30 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import kotlin.properties.Delegates
 
 class GameActivity : AppCompatActivity() {
     val BASE_URL = "https://www.deckofcardsapi.com"
     lateinit var btnLogout: Button
     lateinit var btnBet: Button
+    lateinit var btnDraw: Button
+    lateinit var btnHold : Button
     lateinit var tvPlayerStack: TextView
     lateinit var tvPlayerName: TextView
     lateinit var etPlacedBet: EditText
+
+    //spelarens iv-kort som dras med btnDraw
+    lateinit var playerCard3 : ImageView
+    lateinit var playerCard4 : ImageView
+    lateinit var playerCard5 : ImageView
+
+    //Dealerns iv-kort som börjar dras med btnHold
+    lateinit var dealerCard3 : ImageView
+    lateinit var dealerCard4 : ImageView
+    lateinit var dealerCard5 : ImageView
+
+    //variabel som styr vilket kort som ska få värde i en lista av korten/imageviews
+    var cardCount : Int = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,11 +48,24 @@ class GameActivity : AppCompatActivity() {
 
         getDeck()
 
+        //instansiering av spelarens iv-kort
+        playerCard4 = findViewById(R.id.playerCard4)
+        playerCard5 = findViewById(R.id.playerCard5)
+        playerCard3 = findViewById(R.id.playerCard3)
+
+        //instansiering av dealerns iv-kort
+        dealerCard3 = findViewById(R.id.dealerCard3)
+        dealerCard4 = findViewById(R.id.dealerCard4)
+        dealerCard5 = findViewById(R.id.dealerCard5)
+
+
         btnLogout = findViewById(R.id.btn_logout)
         btnBet = findViewById(R.id.btn_bet)
         tvPlayerStack = findViewById(R.id.tv_stack_size)
         tvPlayerName = findViewById(R.id.tvPlayerName)
         etPlacedBet = findViewById(R.id.et_bet)
+        btnDraw = findViewById(R.id.btn_draw)
+        btnHold = findViewById(R.id.btn_hold)
 
         val currentUser = User(
             intent.getStringExtra("username").toString(),
@@ -56,6 +86,11 @@ class GameActivity : AppCompatActivity() {
             getStartCards(startBtn.tag.toString(), 4)
         }
 
+        btnHold.setOnClickListener{
+            getDrawnCardToDealer(startBtn.tag.toString(), 1, cardCount)
+            cardCount++
+        }
+
         btnBet.setOnClickListener{
             try {
                 etPlacedBet = findViewById(R.id.et_bet)
@@ -66,10 +101,15 @@ class GameActivity : AppCompatActivity() {
             }catch (e: NumberFormatException) {
 
             }
+        }
 
+        btnDraw.setOnClickListener{
+            getDrawnCard(startBtn.tag.toString(), 1, cardCount)
+            cardCount ++
         }
     }
 
+    //Hämtar en kortlek
     private fun getDeck() {
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
@@ -98,7 +138,7 @@ class GameActivity : AppCompatActivity() {
         })
     }
 
-
+    // Hämtar fyra kort, 2 till spelaren och 2 till dealern
     private fun getStartCards(deckId : String, count : Int){
 
         val retrofit = Retrofit.Builder()
@@ -137,7 +177,8 @@ class GameActivity : AppCompatActivity() {
         })
     }
 
-    private fun getDrawnCard(deckId: String, count: Int){
+    //hämtar ett kort i taget och skriver ut kort till spelaren
+    private fun getDrawnCard(deckId: String, count: Int, cardCount : Int){
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
@@ -154,10 +195,42 @@ class GameActivity : AppCompatActivity() {
                 if (deck != null) {
                     println(deck.deck_id + "  remaining: " + deck.remaining)
                     println(deck.cards[0].value)
-
+                    var ivList : MutableList<ImageView> = mutableListOf(playerCard3,playerCard4,playerCard5)
+                        ivList[cardCount].load(deck.cards[0].image)
+                    return
                 }
             }
+            override fun onFailure(call: Call<Deck>, t: Throwable) {
+                Log.d("MainActivity", "did not work $t")
+            }
+        })
+    }
+    //hämtar ett kort i taget och skriver ut kort till dealern
+    private fun getDrawnCardToDealer(deckId: String, count: Int, cardCount : Int){
 
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(RetrofitInterface::class.java)
+
+        val call = retrofit.getDrawnCard(deckId, count)
+
+        //while (dealer.score < 17){
+        call?.enqueue(object: Callback<Deck> {
+            override fun onResponse(
+                call: Call<Deck>,
+                response: Response<Deck>
+            ) {
+                val deck : Deck? = response.body()
+                if (deck != null) {
+                    println(deck.deck_id + "  remaining: " + deck.remaining)
+                    println(deck.cards[0].value)
+                    var ivList : MutableList<ImageView> = mutableListOf(dealerCard3,dealerCard4,dealerCard5)
+                    ivList[cardCount].load(deck.cards[0].image)
+                    return
+                }
+            }
             override fun onFailure(call: Call<Deck>, t: Throwable) {
                 Log.d("MainActivity", "did not work $t")
             }
