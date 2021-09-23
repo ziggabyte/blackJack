34c521c4087ee24807c1b7a97d5269844494e837
ziggi <ziggi.willpower@gmail.com>
2021-09-23 13:31:22 +0200

    add functionality to Logout Button

--- app/src/main/java/com/example/blackjackappx/BettingActivity.kt
+++ app/src/main/java/com/example/blackjackappx/BettingActivity.kt
@@ -10,6 +10,7 @@ class BettingActivity : AppCompatActivity() {
 
     lateinit var btnPlaceBetAndAStart : Button
     lateinit var etBet : EditText
+    lateinit var btnLogout : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +18,7 @@ class BettingActivity : AppCompatActivity() {
 
         btnPlaceBetAndAStart = findViewById(R.id.btn_place_bet_and_start)
         etBet = findViewById(R.id.et_bet)
+        btnLogout = findViewById(R.id.btn_logout)
 
         btnPlaceBetAndAStart.setOnClickListener{
             val placedBet = etBet.text.toString()
@@ -25,6 +27,12 @@ class BettingActivity : AppCompatActivity() {
             intent.putExtra("username", getIntent().getStringExtra("username"))
             intent.putExtra("password", getIntent().getStringExtra("password"))
 
+            startActivity(intent)
+        }
+
+        btnLogout.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("logoutMessage", "Du är utloggad, välkommen åter!")
             startActivity(intent)
         }
     }
