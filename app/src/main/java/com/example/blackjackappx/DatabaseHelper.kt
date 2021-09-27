99609195a02949b90f4bbd89bfc6e6b1ab548095
ziggi <ziggi.willpower@gmail.com>
2021-09-27 15:45:44 +0200

    updates userstack and saves to database at logout

--- app/src/main/java/com/example/blackjackappx/DatabaseHelper.kt
+++ app/src/main/java/com/example/blackjackappx/DatabaseHelper.kt
@@ -4,23 +4,35 @@ import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import com.example.blackjackappx.entities.User
 
 class DatabaseHelper(context : Context?) : SQLiteOpenHelper(context, "UserDatabase.db", null, 1) {
 
-    val USER_TABLE = "USER_TABLE"
-    val COLUMN_USERNAME = "COLUMN_USERNAME"
-    val COLUMN_PASSWORD = "COLUMN_PASSWORD"
+    private val USER_TABLE = "USER_TABLE"
+    private val COLUMN_USERNAME = "COLUMN_USERNAME"
+    private val COLUMN_PASSWORD = "COLUMN_PASSWORD"
+    private val COLUMN_STACK = "COLUMN_STACK"
 
     override fun onCreate(db: SQLiteDatabase?) {
         val query = "CREATE TABLE " +
                 USER_TABLE + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 COLUMN_USERNAME + " TEXT, " +
-                COLUMN_PASSWORD + " TEXT)"
+                COLUMN_PASSWORD + " TEXT, " +
+                COLUMN_STACK + " INTEGER)"
         db?.execSQL(query)
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, p1: Int, p2: Int) {
         //används ej
+    }
+
+    fun saveUserData(user : User) {
+        val db = this.writableDatabase
+        val cv = ContentValues()
+
+        cv.put(COLUMN_STACK, user.userStack)
+
+        db.update(USER_TABLE, cv, "COLUMN_USERNAME = ?", arrayOf(user.username) )
     }
 
     fun isInDatabase(username : String, password : String) : Boolean {
@@ -39,6 +51,6 @@ class DatabaseHelper(context : Context?) : SQLiteOpenHelper(context, "UserDataba
         cv.put(COLUMN_USERNAME, "Testuser")
         cv.put(COLUMN_PASSWORD, "Password123")
 
-        val long = db.insert(USER_TABLE, null, cv)
+        db.insert(USER_TABLE, null, cv)
     }
 }
